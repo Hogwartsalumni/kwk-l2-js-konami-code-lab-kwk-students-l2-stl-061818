3869907ad1664d297529f61b15cae8810dda3751
Scarlett Patton <scarwars05@gmail.com>
2018-06-25 13:40:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,27 @@ const codes = [
   "b",
   "a"
 ];
+ 
+let index = 0;
 
 function init() {
   // your code here
+  const key = e.key;
+  if (key === codes[i]) {
+    index++;
+    if (index === codes.length) {
+      alert('Hurray!');
+      
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
+
+  function triggerKeyDown(key) {
+    const keyboardEvent = new KeyboardEvent("keydown", { key });
+    document.body.dispatchEvent(keyboardEvent);
+  }
+  
+  init();
